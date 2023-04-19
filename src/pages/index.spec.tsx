d056265069f86121b58f5d8c830464d71acddfad
Dmitry Frolov <dfrolov@asdco.ru>
2023-04-19 21:24:51 +0200

    added test environment

---
+++ src/pages/index.spec.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+
+import Home from './index';
+
+describe('<Home />', () => {
+  it('renders component', () => {
+    render(
+      <Home />
+    );
+    expect(screen.getByText('Get started by editing')).toBeInTheDocument();
+  });
+});
