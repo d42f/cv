06b7f48d046468939d06023e34fb2a063819dc23
Dmitry Frolov <dfrolov@asdco.ru>
2023-04-21 23:28:47 +0200

    test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Logo } from './Logo';
 import { Menu } from './Menu';
 
 import styles from './Header.module.scss';
 
-export const Header = (): JSX.Element => (
-  <header className={styles.wrapper}>
-    <Logo />
-    <Menu className={styles.menu} />
-  </header>
-);
+export const Header = (): JSX.Element => {
+  const a = useState(1);
+
+  useEffect(() => {
+    console.log({ a });
+  }, []);
+
+  return (
+    <header className={styles.wrapper}>
+      <Logo />
+      <Menu className={styles.menu} />
+    </header>
+  );
+};
