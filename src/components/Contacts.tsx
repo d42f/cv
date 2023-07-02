d4c86abf1bf98a3351488b72ef0f1bd503ee8f52
d42f <dmitri.frolof@gmail.com>
2023-07-02 22:44:47 +0200

    refactored contacts

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,13 +1,15 @@
 import classNames from 'classnames';
-import { FaEnvelope, FaTelegramPlane } from 'react-icons/fa';
+import { FaEnvelope, FaPhone, FaTelegramPlane } from 'react-icons/fa';
 
-import { email, telegram } from '@/resume';
-import { linkEmail, linkTelegram } from '@/utils/link';
+import { email, phone, telegram } from '@/resume';
+import { linkEmail, linkPhone, linkTelegram } from '@/utils/link';
+import { formatPhone } from '@/utils/phone';
 import styles from './Contacts.module.scss';
 
 const CONTACTS = [
-  ...(telegram ? [{ label: `@${telegram}`, href: linkTelegram(telegram), Icon: FaTelegramPlane }] : []),
   { label: email, href: linkEmail(email), Icon: FaEnvelope },
+  ...(phone ? [{ label: formatPhone(phone), href: linkPhone(phone), Icon: FaPhone }] : []),
+  ...(telegram ? [{ label: `@${telegram}`, href: linkTelegram(telegram), Icon: FaTelegramPlane }] : []),
 ];
 
 interface ContactsProps {
@@ -17,8 +19,9 @@ interface ContactsProps {
 export const Contacts = ({ className }: ContactsProps): JSX.Element => (
   <div className={classNames(styles.wrapper, className)}>
     <p className={styles.description}>
-      Want to say hello? Want to know more about me? Give me a call or drop me an email and I will get back to you as
-      soon as I can.
+      Want to say hello? Want to know more about me?
+      <br />
+      Drop me an email and I will get back to you as soon as I can.
     </p>
     <div className={styles.info}>
       {CONTACTS.map(({ label, href, Icon }, index) => (
