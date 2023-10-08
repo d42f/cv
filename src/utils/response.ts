9dbd19bc6d5ab618aafecf05f3e2663e7dc5c821
d42f <dmitri.frolof@gmail.com>
2023-10-08 12:05:56 +0200

    fixed invalid import

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,4 +1,4 @@
-import { IResponseStatus, IServerError } from '@/models/IResponse';
+import { IErrorResponse, IResponseStatus } from '@/models/IResponse';
 
-export const isServerError = (response: unknown): response is IServerError =>
+export const isServerError = (response: unknown): response is IErrorResponse =>
   !!response && typeof response === 'object' && 'status' in response && response.status === IResponseStatus.Error;
